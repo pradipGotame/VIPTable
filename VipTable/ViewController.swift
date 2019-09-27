689c0990b15188a780dce6c59939a4d9b9ac0ae0
Pradip Gotame <pradip.gotame@hitchedapp.com>
2019-09-27 13:27:33 +0545

    Initial Commit

---
+++ VipTable/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VipTable
+//
+//  Created by Pradip Gotame on 9/27/19.
+//  Copyright © 2019 Pradip Gotame. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
